aac49b986e24f485a0c8c8ea7f9e5ffd9af51247
hisashi kominami <komi73tov@gmail.com>
2023-08-10 12:53:25 +0000

    first commit

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -1,12 +1,25 @@
 class TopicsController < ApplicationController
   def new
+    @topic = Topic.new
     render :new
   end
 
 
 def create
-  redirect_to edit_topics_path
+  @topic = Topic.new(topic_params)
+  
+  if @topic.save
+    redirect_to index_post_path, notice: '登録しました'
+  else
+    render :new, status: :unprocessable_entity
+  end
+  
 end
+
+private
+  def topic_params
+    params.require(:topic).permit(:title)
+  end
 
 def edit
   render :edit
---
+++ app/models/topic.rb
@@ -0,0 +1,6 @@
+class Topic < ApplicationRecord
+  
+  
+  validates :title, length: { maximum: 100 },presence:true
+  
+end
