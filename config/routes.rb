3c49b8685e5235cae5b6a14b31b556d901d9205a
hisashi kominami <komi73tov@gmail.com>
2023-08-13 08:31:08 +0000

    2-13完了

--- config/routes.rb
+++ config/routes.rb
@@ -16,6 +16,10 @@ Rails.application.routes.draw do
   
   delete 'posts/destroy/:id', to: 'posts#destroy', as: 'destroy_post'
   
+  #CommentsController
+  get 'posts/show/:post_id/comments/new', to: 'comments#new', as: 'new_comment'
+  post 'posts/show/:post_id/comments/new', to: 'comments#create', as: 'create_comment'
+  
   #TopicsController
   get 'topics/new',to: 'topics#new',as: 'new_topic'
   
